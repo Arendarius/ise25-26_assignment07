2082e21996890f23d4fa711c4fae9398d0f1c4d8
Arendarius <arendar.daniel@gmail.com>
2025-12-11 17:30:18 +0100

    dto gemacht und mapper entkommentiert

--- api/src/main/java/de/seuhd/campuscoffee/api/dtos/ReviewDto.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/dtos/ReviewDto.java
@@ -3,15 +3,25 @@ package de.seuhd.campuscoffee.api.dtos;
 import lombok.Builder;
 import org.jspecify.annotations.Nullable;
 
+import java.time.LocalDateTime;
+
 /**
  * DTO record for POS metadata.
  */
 @Builder(toBuilder = true)
 public record ReviewDto (
-    @Nullable Long id
-    // TODO: Implement ReviewDto
+        @Nullable Long id,
+        @Nullable LocalDateTime createdAt, // is null when using DTO to create a new POS
+        @Nullable LocalDateTime updatedAt, // is set when creating or updating a POS
+        // TODO: Implement ReviewDto
+        @Nonnull Long posId,
+        @Nonnull Long userId,
+        @NotBlank(message = "Review cannot be empty.")
+        @Nonnull String review,
+        @Min(value = 0, message = "Approval count must be greater than or equal to 0.")
+        @Nonnull Integer approvalCount,
+        @Nonnull Boolean approved
 
-    @Nonnull Boolean false
 
 ) implements Dto<Long> {
     @Override
